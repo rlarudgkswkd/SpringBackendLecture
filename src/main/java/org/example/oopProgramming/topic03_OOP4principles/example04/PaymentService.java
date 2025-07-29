6b044063d1f643007e57d37c904510b063e1aeb4
goregoreda <rlarudgkswkd@gmail.com>
2025-07-29 13:24:38 +0900

    oop4principle finished

---
+++ src/main/java/org/example/oopProgramming/topic03_OOP4principles/example04/PaymentService.java
@@ -0,0 +1,14 @@
+package org.example.oopProgramming.topic03_OOP4principles.example04;
+
+
+public class PaymentService {
+    public void processPayment(UserAccount acc, double amount) {
+        acc.withdraw(amount);
+        System.out.println("결제 완료. 잔액: " + acc.getBalance());
+    }
+
+    public void processRefund(UserAccount acc, double amount) {
+        acc.deposit(amount);
+        System.out.println("환불 완료. 잔액: " + acc.getBalance());
+    }
+}
