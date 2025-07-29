3869ac5cfb498946ea794b07fb93b03235121b3a
goregoreda <rlarudgkswkd@gmail.com>
2025-07-29 14:21:50 +0900

    innerclass and anonymous class

---
+++ src/main/java/org/example/oopProgramming/topic05_InnerClassAndAnonymousClass/example02_NonStaticInnerClass/Outer.java
@@ -0,0 +1,13 @@
+package org.example.oopProgramming.topic05_InnerClassAndAnonymousClass.example02_NonStaticInnerClass;
+
+public class Outer {
+    private int instanceValue = 200;
+
+    // 비정적 내부 클래스
+    public class Inner {
+        public void printInstanceValue() {
+            System.out.println("Outer instanceValue: " + instanceValue);
+        }
+    }
+}
+
