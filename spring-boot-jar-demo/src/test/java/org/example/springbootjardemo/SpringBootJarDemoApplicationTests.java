f1b4696842e4fb4e50dc494707d898efa54bf9c4
goregoreda <rlarudgkswkd@gmail.com>
2025-08-06 13:03:04 +0700

    jar demo project

---
+++ spring-boot-jar-demo/src/test/java/org/example/springbootjardemo/SpringBootJarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootjardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
