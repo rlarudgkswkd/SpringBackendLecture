c714f2cdc120042a7c1ae575baca1eaf6d31e71a
goregoreda <rlarudgkswkd@gmail.com>
2025-12-08 12:44:10 +0700

    2025-12-08-02

---
+++ SpringBootProjectStructureExample/src/main/java/org/example/springbootprojectstructureexample/SpringBootProjectStructureExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootprojectstructureexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectStructureExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectStructureExampleApplication.class, args);
+	}
+
+}
---
+++ SpringBootProjectStructureExample/src/test/java/org/example/springbootprojectstructureexample/SpringBootProjectStructureExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootprojectstructureexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectStructureExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
