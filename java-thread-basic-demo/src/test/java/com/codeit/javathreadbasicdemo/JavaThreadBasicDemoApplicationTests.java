f405f379f7ad0584ee6cc6a7b2331df4261380f7
goregoreda <rlarudgkswkd@gmail.com>
2026-05-25 12:01:57 +0700

    java async-Java Thread Basic-pratice01

---
+++ java-thread-basic-demo/src/test/java/com/codeit/javathreadbasicdemo/JavaThreadBasicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.javathreadbasicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadBasicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
