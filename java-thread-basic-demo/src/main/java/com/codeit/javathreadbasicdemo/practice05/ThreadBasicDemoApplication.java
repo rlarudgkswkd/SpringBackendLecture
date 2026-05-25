5273a7b5c716d75875056329107efd471f1d65f3
goregoreda <rlarudgkswkd@gmail.com>
2026-05-25 13:56:09 +0700

    java async-Java Thread Basic-pratice05

---
+++ java-thread-basic-demo/src/main/java/com/codeit/javathreadbasicdemo/practice05/ThreadBasicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.javathreadbasicdemo.practice05;
+
+public class ThreadBasicDemoApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java Thread 기초와 동기화 실습 시작");
+
+        SynchronizedComparisonDemo.run();
+
+        System.out.println("실습 종료");
+    }
+}
