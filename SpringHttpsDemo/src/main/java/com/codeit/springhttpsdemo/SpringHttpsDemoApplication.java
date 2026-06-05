5202443118b5dd9dade7aad92939307a46d0d0f5
goregoreda <rlarudgkswkd@gmail.com>
2026-06-05 14:55:26 +0700

    mkcert practice

---
+++ SpringHttpsDemo/src/main/java/com/codeit/springhttpsdemo/SpringHttpsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.springhttpsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHttpsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHttpsDemoApplication.class, args);
+    }
+
+}
